ea98411130961c0dda47fceb9cfe0b936386324c
Kishore <kishore22705@gmail.com>
2025-04-12 17:32:19 +0530

    12th of april changes

--- components/providers/theme-provider.tsx
+++ components/providers/theme-provider.tsx
@@ -1,9 +1,19 @@
-"use client";
-
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import { ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  // Ensure `attribute` is properly typed
+  const { attribute, defaultTheme, enableSystem, disableTransitionOnChange } = props;
+  
+  return (
+    <NextThemesProvider
+      attribute={attribute} 
+      defaultTheme={defaultTheme} 
+      enableSystem={enableSystem} 
+      disableTransitionOnChange={disableTransitionOnChange}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
